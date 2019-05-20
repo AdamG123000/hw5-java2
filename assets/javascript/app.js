0e2ea989dd7a9775424a1daa510894873235b90b
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-05-20 15:24:45 -0700

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@
 //each qusetion has four asnwers to select, only one can selected at a time
 //create a finish button that presents new page with number of correct, incorrect, unasswered questions 
 
+
+//create timer the counts down from 30 seconds
 var counter = 30;
 var intervalID;
 
@@ -17,17 +19,140 @@ function beginTimer() {
         counter--;
         $("#timer").html("<h2>" + counter + "<h2>");
         if (counter === 0) {
-            finish();
+            finishQuiz();
             alert("Times Up");
         } 
     } 
 }
-function finish(){
+function finishQuiz(){
     clearInterval(intervalID);
+    //display results page
 }
 // beginTimer(); 
 $("#startButton").on('click', beginTimer);
-$("#finish-button").on('click', finish);
+$("#finish-button").on('click', finishQuiz);
+
+
+//filling out the quiz
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const finishButton = document.getElementById('sumbit');
+
+function buildQuiz(){
+    const output = [];
+    //for each qustion
+    quizQuestions.forEach(
+        (_currentQuestion, _questionNumber) => {
+            const answers = [];
+            for (lettre in _currentQuestion.answers){
+                answers.push(<label>
+                    <input type="radio" name="question ${questionNumber}" value="${letter}">
+                </label>);
+            }
+        }
+    )
+}
+
+function showResults(){}
+
+//display quiz when page loads
+buildQuiz();
+
+//show results when submit button is clicked
+submitButton.addEventListener('click', showResults);
+
+//display quiz questions 
+const quizQuestions = [
+    {
+        question: "What animal is largest?",
+        answers: {
+            a: "Whale",
+            b: "Dophin",
+            c: "Rat",
+            d: "Chinchilla"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "What animal is blue?",
+        answers: {
+            a: "Black Panther",
+            b: "Red Panda",
+            c: "Blue Whale",
+            d: "Unicorn"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        question: "What animal lives in the water?",
+        answers: {
+            a: "Human",
+            b: "Bear",
+            c: "Wolf",
+            d: "Fish"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Which animal is an ape?",
+        answers: {
+            a: "Bigfoot",
+            b: "Dragon",
+            c: "Rhino",
+            d: "Hammerhead Shark"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        question: "What animal eats other animals?",
+        answers: {
+            a: "Horse",
+            b: "Tiger",
+            c: "Goat",
+            d: "Elephant"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "What animal has no limbs?",
+        answers: {
+            a: "Lion",
+            b: "Monkey",
+            c: "Lizard",
+            d: "Snake"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        question: "Which animal can code this quiz?",
+        answers: {
+            a: "Orangutan",
+            b: "Human",
+            c: "Chimpanzee",
+            d: "Gorilla"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        question: "What animal makes the best pet?",
+        answers: {
+            a: "Elephant",
+            b: "Platapus",
+            c: "Dog",
+            d: "Lion"
+        },
+        correctAnswer: "c"
+    }
+];
+
+buildQuiz();
 
 
 
@@ -35,49 +160,4 @@ $("#finish-button").on('click', finish);
 
 
 
-
-// document.ready(function(){
-//     $("remaining-time").hide();
-//     $("#start-button").on('click', trivia.startGame);
-//     $(document).on('click', 'option', trivia.guessChecker);
-// });
-
-// var count = 15;
-// var interval = setInterval(function(){
-//   document.getElementById('startButton').innerHTML=count;
-//   count--;
-//   if (count === 0){
-//     clearInterval(interval);
-//     document.getElementById('startButton').innerHTML='Done';
-//     // or...
-//     alert("You're out of time!");
-//   }
-// }, 1000);
-
-
-
-// var trivia = [{
-
-//     correct: 0,
-//     incorrect: 0,
-//     unanswered: 0,
-//     currentSet: 0,
-//     timer: 20,
-//     timerOn: false,
-//     timerId: '',
-
-//     questions: {
-
-
-
-//     "question": "Which animal is the biggest?",
-//     "option-1": "Whale",
-//     "option-2": "Giraffe",
-//     "option-3": "Bear",
-//     "option-4": "Zebra",
-//     "answer": "1",
-//     }
-// }];
-
-//for loop
 
